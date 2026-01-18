6c5619bdff0e52aaff4913c2fc59cedd33577af7
Allan Miljan <allan.miljan@tptlive.ee>
2026-01-18 23:00:47 +0200

    simple CRUD

--- example/resources/views/jobs/show.blade.php
+++ example/resources/views/jobs/show.blade.php
@@ -6,4 +6,7 @@
     <h2>{{ $job['title'] }}</h2>
 
     <p>This Job pays: {{ $job['salary'] }} </p>
+
+    <p class="mt-6">
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
 </x-layout>
